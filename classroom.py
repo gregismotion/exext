b2b564442c9198c027fbd1732cc0784799032f2e
gergovari <work@varigergo.hu>
2023-07-22 09:18:46 +0200

    refactor: abstart classroom

--- classroom.py
+++ classroom.py
@@ -31,3 +31,31 @@ class ClassroomHandler:
 			announcements += response.get("announcements", [])
 			request = self.service.courses().announcements().list_next(request, response)
 		return announcements
+
+	def _assignments_to_materials(self, assignments):
+		materials = []
+		for assignment in assignments:
+			try:
+				materials += assignment["materials"]
+			except KeyError:
+				continue
+		return materials
+	def _materials_to_files(self, materials):
+		files = []
+		for material in materials:
+			try:
+				files.append(material["driveFile"]["driveFile"])
+			except KeyError:
+				continue
+		return files
+	def _files_to_docs(self, files):
+		docs = []
+		print()
+		for i, file in enumerate(files):
+			print(f"Downloading {i+1}/{len(files)} ({round(((i+1)/len(files))*100, 2)}%): {file['title']}")
+			docs.append(pdfmng.blob_to_pdf(drive.get_file_as_pdf(file)))
+		return docs
+	def assignments_to_docs(self, assignments):
+		return self._files_to_docs(self._materials_to_files(self._assignments_to_materials(assignments)))
+
+
--- main.py
+++ main.py
@@ -9,32 +9,7 @@ from pdfmanager import PDFManager
 from datetime import datetime
 from dateutil import parser
 
-# TODO: filter by title
-def assignments_to_materials(assignments):
-	materials = []
-	for assignment in assignments:
-		try:
-			materials += assignment["materials"]
-		except KeyError:
-			continue
-	return materials
-def materials_to_files(materials):
-	files = []
-	for material in materials:
-		try:
-			files.append(material["driveFile"]["driveFile"])
-		except KeyError:
-			continue
-	return files
-def files_to_docs(files):
-	docs = []
-	print()
-	for i, file in enumerate(files):
-		print(f"Downloading {i+1}/{len(files)} ({round(((i+1)/len(files))*100, 2)}%): {file['title']}")
-		docs.append(pdfmng.blob_to_pdf(drive.get_file_as_pdf(file)))
-	return docs
-
-# TODO: filter by title
+# TODO: filter config
 def filter_assignments(assignments):
 	cutoff = datetime(2023, 6, 1)
 	return [assignment for assignment in assignments if parser.parse(assignment["creationTime"]).timestamp() >= cutoff.timestamp()]
@@ -55,9 +30,7 @@ def choose_docs(classroom, drive, pdfmng):
 	course = choose_course()
 	assignments = filter_assignments(classroom.get_assignments(course) + classroom.get_announcements(course))
 	print(f"Assignments: {len(assignments)}")
-	materials = assignments_to_materials(assignments)
-	files = materials_to_files(materials)
-	return files_to_docs(files)
+	return classroom.assignments_to_docs(assignments)
 
 def merge_docs_to_pdf(extractor, merger, docs, output):
 	exercises = extractor.extract_all(docs, include_titles = True)
